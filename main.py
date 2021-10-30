022b655af708aefa65f2e63c04da53481be058fc
Caden Ertel <cadenertel@gmail.com>
2021-10-30 13:45:06 -0400

    testing changes
    Co-authored-by: Tunchiie <Tunchiie@users.noreply.github.com>
    Co-authored-by: Christopher Slaughter <christopherslaughter@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 """
 Main Page for HackUMBC
+Name: SizeUp
+Authors: Caden Ertel, Christopher Slaughter, Adetunji Fasiku
+Discription:
 """
-
 import streamlit as st
 
+
+st.title('SizeUp!')
+st.subheader('A personal fitness community at your fingertips')
+
+
+col1, col2, col3 = st.columns(3)
+
+# this will put a button in the middle column
+with col1:
+    st.button('Login')
+
+with col3:
+    st.button('Create an Account')
